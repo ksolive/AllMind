d9a642c0c99f532d8c82152b0424a47d11004bc9
ksolive <wyh1004@outllok.com>
2023-12-25 11:51:05 +0800

    活动窗口监视器适配了mac

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 from datetime import datetime
 
@@ -6,10 +7,39 @@ try:
     platform = "Windows"
 except ImportError:
     platform = "macOS"
-    from AppKit import NSWorkspace
+    import subprocess
 
-def get_active_window_title_mac():
-    return NSWorkspace.sharedWorkspace().frontmostApplication().localizedName()
+def get_active_window_info_mac():
+    script = """
+    global frontApp, frontAppName, windowTitle
+
+    set windowTitle to ""
+    tell application "System Events"
+        set frontApp to first application process whose frontmost is true
+        set frontAppName to name of frontApp
+        set windowTitle to "no window"
+        tell process frontAppName
+            if exists (1st window whose value of attribute "AXMain" is true) then
+                tell (1st window whose value of attribute "AXMain" is true)
+                    set windowTitle to value of attribute "AXTitle"
+                end tell
+            end if
+        end tell
+    end tell
+
+    return {frontAppName, windowTitle}
+    """
+
+    try:
+        result = subprocess.run(['osascript', '-e', script], capture_output=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout.strip()
+            print(output)
+            return output
+        else:
+            return "Error executing AppleScript"
+    except Exception as e:
+        return f"Error: {e}"
 
 def get_active_window_title_windows():
     return win32gui.GetWindowText(win32gui.GetForegroundWindow())
@@ -18,7 +48,7 @@ def get_active_window_title_windows():
 if(platform == "Windows"):
     get_active_window_title = get_active_window_title_windows
 else:
-    get_active_window_title = get_active_window_title_mac
+    get_active_window_title = get_active_window_info_mac
 
 def write_log_to_file(logfile_path, log_entries):
     with open(logfile_path, 'a') as log_file:
